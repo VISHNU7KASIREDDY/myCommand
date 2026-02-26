2e70d3e798cc22c48528fbef71f46a5d66fc1ac5
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2026-02-26 09:49:50 +0530

    init

---
+++ cli.ts
@@ -0,0 +1,9 @@
+const {Command}=require("commander")
+
+const program=new Command()
+
+program
+.command("greet <name>")
+.action((name)=>{console.log(`Hello ${name}`)})
+
+program.parse()
---
+++ dist/cli.js
@@ -0,0 +1,6 @@
+const { Command } = require("commander");
+const program = new Command();
+program
+    .command("greet <name>")
+    .action((name) => { console.log(`Hello ${name}`); });
+program.parse();
