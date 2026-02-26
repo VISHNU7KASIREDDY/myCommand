6c4a8d78a1a1a41f7acd6a520d29fad495c883c6
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2026-02-26 10:04:34 +0530

    added basic arithmetic operations

--- dist/cli.js
+++ dist/cli.js
@@ -1,6 +1,23 @@
+#!/usr/bin/env node
 const { Command } = require("commander");
 const program = new Command();
 program
     .command("greet <name>")
     .action((name) => { console.log(`Hello ${name}`); });
+program
+    .command("add <n1> <n2>")
+    .description("Add two numbers")
+    .action((n1, n2) => { console.log(Number(n1) + Number(n2)); });
+program
+    .command("subtract <n1> <n2>")
+    .description("Subtract two numbers")
+    .action((n1, n2) => { console.log(Number(n1) - Number(n2)); });
+program
+    .command("multiply <n1> <n2>")
+    .description("Multiply two numbers")
+    .action((n1, n2) => { console.log(Number(n1) * Number(n2)); });
+program
+    .command("divide <n1> <n2>")
+    .description("Divide a with b")
+    .action((n1, n2) => { console.log(Number(n1) - Number(n2)); });
 program.parse();
